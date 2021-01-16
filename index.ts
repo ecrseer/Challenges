706115f255eadd1c78a3a0f17ec3b0d32c1999d1
ecrseer <juustino@live.com>
2021-01-16 01:39:58 -0300

    fix: primeiro parametro do slice

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 let 
     valo: string[]=["1, 3, 4, 13, 32", "1, 2, 4, 13, 15, 32"],    
     finalValor:number,
+    iniciValor:number=0,
     temos:string='';
 
     valo = valo.map(seq=>" "+seq+",");
@@ -11,9 +12,13 @@ while(finalValor!=-1){
     
     let
       varPrimeiroArr: string = valo[0]
-        .slice(finalValor-2,finalValor+1),
-        indxNumero: number = valo[1].indexOf(varPrimeiroArr);
-        console.log('fval: '+finalValor);
+        .slice(iniciValor+1,finalValor+1);//0,3
+
+        
+    let indxNumero: number 
+        = valo[1].indexOf(varPrimeiroArr);
+        console.log('prim: '+varPrimeiroArr);
+        console.log('iniciValor: '+iniciValor);
 
        if(indxNumero!=-1){
         let virg:number = 
@@ -22,8 +27,8 @@ while(finalValor!=-1){
             valo[1].slice(indxNumero,virg )+',';
         }
 
-
       finalValor=valo[0].indexOf(',',finalValor+1);
+      iniciValor=valo[0].indexOf(' ',iniciValor+1);
       console.log('fina eh'+finalValor);     
     
 }console.log('temos: '+temos);
