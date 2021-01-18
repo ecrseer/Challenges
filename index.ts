7d8fad38462efd448c700d95c01013b22e49a072
ecrseer <juustino@live.com>
2021-01-18 13:12:09 -0300

    skip for inside

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 function twoSum(nums: number[], target: number): number[] {
     
-    nums = nums.filter(num=>num<target);
+    nums = nums.filter(num=>num<target);  
+
+    if(nums[0]+nums[1]==target)
+        return [0,1];
 
     for (var _i = 0; _i < nums.length; _i++) {
-        for (var _y = 0; _y < nums.length; _y++) {                        
-            if(target-(nums[_i]+nums[_y])==0){
+        for (var _y = 0; _y < nums.length; _y++) {             
+            if((nums[_i]+nums[_y]==target)&&
+                (_y!=_i)
+                ){
                 return [_i,_y];
             }
           }
       }
 
-    return nums;
+    return[-1,-1];
+    
 };
 
 console.log(
-    twoSum([2,7,11,15],9)
+    twoSum([3,3],4)
 );
