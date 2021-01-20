8517d7ba54ab2c5c3ead3334a0984ce5715729b2
ecrseer <juustino@live.com>
2021-01-20 00:36:29 -0300

    done

--- index.ts
+++ index.ts
@@ -1,27 +1,30 @@
-function twoSum(nums: number[], target: number): number[] {
+function numeroEmArray(vl:number):number[]{
+    let
+        restos:number[] = [vl%10];
+        vl=Math.floor(vl/10)
+        while(vl>8){
+            restos.push(vl%10);
+            vl=Math.floor(vl/10)
+        }
+        restos.push(vl);
+        
+        return restos;       
     
-    
-
-    if(nums[0]+nums[1]==target)
-        return [0,1];
-
-    for (var _i = 0; _i < nums.length; _i++) {
-        console.log(nums[_i]);
-        for (var _y = 0; _y < nums.length; _y++) {             
-            
-            if((nums[_i]+nums[_y]==target)&&
-                (_y!=_i)
-                ){
-                    
-                return [_i,_y];
-            }
-          }
-      }
-
-    return[-1,-1];
+}
+function potencia(_y:number,indi:number):number{
+    let pot:number=10**(numeroEmArray(_y).length-indi)
+    return pot;
+}
+function reverse(x: number): number {
+    console.log(numeroEmArray(x));
+    let mvl:number=
+    numeroEmArray(x).reduce((acumul,atualV,indice)=>{        
+       return acumul+atualV*potencia(x,indice+1);
+    },0);
+    return  mvl;
     
 };
 
-console.log(
-    twoSum([0,4,3,0],0)
+console.log(//4623
+    reverse(3264)
 );
