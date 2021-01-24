0c433de61506931c175d170e54fc43961ba1169d
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2021-01-24 00:52:38 -0300

    first

--- index.ts
+++ index.ts
@@ -1,39 +1,27 @@
-function numeroEmArray(vl:number):number[]{
-    vl=vl<0?
-        vl*-1 :
-        vl;
-    let
-        restos:number[]=[];
-
-        while(vl>9){
-            restos.push(vl%10);
-            
-            vl=Math.floor(vl/10)
-        }
-        restos.push(vl);
-        
-        return restos;       
-    
+function _whichTextoParaNumero(numero:string):number{
+    switch(numero){
+        case '1': return 1;
+    }
 }
-function potencia(_y:number,indi:number):number{
-    let pot:number=10**(numeroEmArray(_y).length-indi)
-    return pot;
-}
-function reverse(x: number): number {
 
-    
-    let mvl:number=
-    numeroEmArray(x).reduce((acumul,atualV,indice)=>{     
-        
-       return acumul+atualV*potencia(x,indice+1);
-    },0);
-    if(mvl>2**31||mvl<2**-31)
-        mvl=0;
-    return  x<0? -1*mvl:mvl;
-    
+function myAtoi(s: string): number {
+    let i:number=0,
+     nCompleto:number[]=[];
+   while(s.substring(i,i+1)==' '){
+        i++;
+   }
+   
+     while(_whichTextoParaNumero(
+         s.substring(i+1,i+2))!=10){
+             nCompleto.push(
+            _whichTextoParaNumero(
+                s.substring(i+1,i+2)))
+                i++;
+         }
+   return nCompleto.join('').
 };
 
 console.log(//4623
-    reverse(1534236469)
+    myAtoi('')
              
 );
